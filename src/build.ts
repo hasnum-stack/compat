bdb7e710a79b1f9b1847dce5dea7606eefdf6d32
hasnum <hasnum@163.com>
2025-03-24 10:36:45 +0800

    update build process and configuration for improved formatting and browser support

--- src/build.ts
+++ src/build.ts
@@ -1,9 +1,12 @@
-//使用Bun运行时重写
-export const build = ({ list, targets }) => {
+export const build = ({ list, targets, browserslist }) => {
+  // 目标文件
   const distDir = `dist/result_${+new Date()}`;
+
   const targetsDir = `${distDir}/targets.json`;
   const listDir = `${distDir}/list.json`;
+  const browserslistDir = `${distDir}/browserslist.json`;
 
   Bun.write(targetsDir, JSON.stringify(targets));
   Bun.write(listDir, JSON.stringify(list));
+  Bun.write(browserslistDir, JSON.stringify(browserslist));
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import compat from "core-js-compat";
 
 import { build } from "./build";
+
+const browserslist = "since 2020 and not dead";
 const { list, targets } = compat({
-  targets:
-    "> 0.2%, chrome >= 71, firefox >= 62, edge >= 79, safari >= 13, ios_saf >= 13, not dead",
+  // targets:
+  //   "> 0.2%, chrome >= 71, firefox >= 62, edge >= 79, safari >= 13, ios_saf >= 13, not dead",
+  targets: browserslist,
 });
 
-build({ list, targets });
+build({ list, targets, browserslist });
